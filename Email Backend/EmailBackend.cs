9d50f91f0f973d4626ebcf928c35fd76e1ac3a5a
roman015 <xaviervarricatt@live.in>
2018-01-24 11:21:51 +0800

    Email Service Configured

--- Email Backend/EmailBackend.cs
+++ Email Backend/EmailBackend.cs
@@ -10,35 +10,51 @@ namespace Email_Backend
 {
     public class EmailBackend
     {
-        private static string senderSmtpAddress = "smtp.nus.edu.sg";
+        private static bool SendEmailForReal = false;
+
+        private static string senderSmtpAddress = "smtp.sendgrid.net";
         private static int senderSmtpPort = 587;
-        private static string senderEmail = "sender@nus.com";
-        private static string senderUsername = @"your_domain\your_userid";
-        private static string senderPassword = "******";
+        private static string senderEmail = "e0224940@u.nus.edu";
+        private static string senderUsername = @"apikey";
+        private static string senderPassword = "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";
 
         // WARNING : THIS FUNCTION IS UNTESTED!
-        public static void sendEmailStep(String recipientEmail, String emailSubject, String emailContent)
+        public static bool sendEmailStep(String recipientEmail, String emailSubject, String emailContent)
         {
-            new Thread(() =>
-           {
-               try
-               {
-                   SmtpClient client = new SmtpClient(senderSmtpAddress, senderSmtpPort);
-                   client.Credentials = new System.Net.NetworkCredential(senderUsername, senderPassword);
-                   client.EnableSsl = true;
-                   client.DeliveryMethod = SmtpDeliveryMethod.Network;
-                   MailMessage mm = new MailMessage(senderEmail, recipientEmail);
-                   mm.Subject = emailSubject;
-                   mm.Body = emailContent;
-                   client.Send(mm);
-               }
-               catch (Exception e)
-               {
-                   Console.Error.WriteLine(e.Message);
-               }
+            // Default to true if we are not sending email for real
+            bool result = false; 
 
-           }
-                ).Start();
+            if (SendEmailForReal)
+            {
+                try
+                {
+                    SmtpClient client = new SmtpClient(senderSmtpAddress, Convert.ToInt32(senderSmtpPort));
+                    client.Credentials = new System.Net.NetworkCredential(senderUsername, senderPassword);
+                    client.EnableSsl = true;
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.Timeout = 10000;
+
+                    MailMessage mm = new MailMessage(senderEmail, recipientEmail);
+                    mm.Subject = emailSubject;
+                    mm.Body = emailContent;
+
+                    client.Send(mm);
+
+                    result = true;
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine(e.Message);
+                    result = false;
+                }
+            }
+            else
+            {
+                // We are not actually sending email, just passing success for now
+                result = true;
+            }
+
+            return result;
         }
     }
 }
---
+++ LUSSIS/Admin/SendTestEmail.aspx.cs
@@ -0,0 +1,38 @@
+﻿using Email_Backend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Admin_NewAccount : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+
+    protected void SendEmailButton_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            if (EmailBackend.sendEmailStep(
+                Email.Text,
+                Subject.Text,
+                Message.Text
+                ))
+            {
+                StatusText.Text = "[Message Sent Successfully]";
+            }
+            else
+            {
+                StatusText.Text = "[There was a problem sending that email]";
+            }
+        }
+        catch (Exception ex)
+        {
+            StatusText.Text = "[An Exception has occured :] <br/>" + ex.Message;
+        }
+    }
+}
