a8be3f91715b1aae4317cef51b0dfa0fa226e672
Leon <llw1989@gmail.com>
2018-01-24 16:00:32 +0800

    One more file

--- LUSSIS Backend/CrystalReportController.cs
+++ LUSSIS Backend/CrystalReportController.cs
@@ -9,33 +9,33 @@ namespace LUSSIS_Backend
 {
     public class CrystalReportController
     {
-        public static IList populateCrystalReport(string supplierSelected)
-        {
-            LussisEntities context = new LussisEntities();
+//        public static IList populateCrystalReport(string supplierSelected)
+//        {
+//            LussisEntities context = new LussisEntities();
 
-            var result = context.ReorderTrendViews
-    .Where(x => x.SupplierName == supplierSelected)
-        .GroupBy(x => x.Description)
-        .Select( y => new
-        {
-            Description = y.Description,
-            Qty = y.Sum(x => x.Qty)
-        })
-.ToList();
+////            var result = context.ReorderTrendViews
+////    .Where(x => x.SupplierName == supplierSelected)
+////        .GroupBy(x => x.Description)
+////        .Select( y => new
+////        {
+////            Description = y.Description,
+////            Qty = y.Sum(x => x.Qty)
+////        })
+////.ToList();
 
 
-            //var result = context.ReorderTrendViews
-            //    .Where(x => x.SupplierName == supplierSelected)
-            //    .GroupBy(x => new { x.Description, x.Qty })
-            //    .Select(x => new 
-            //    {
-            //        Description = x.Description,
-            //        Qty = x.Qty ?? 0,
-            //    })
-            //.ToList();
+//            //var result = context.ReorderTrendViews
+//            //    .Where(x => x.SupplierName == supplierSelected)
+//            //    .GroupBy(x => new { x.Description, x.Qty })
+//            //    .Select(x => new 
+//            //    {
+//            //        Description = x.Description,
+//            //        Qty = x.Qty ?? 0,
+//            //    })
+//            //.ToList();
 
-            return result;
-        }
+//            return result;
+//        }
 
     }
 }
