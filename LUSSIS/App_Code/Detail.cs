b81f5713c6127b2ef79445abe7981d8286db6b56
Thet Thet <Thet Thet@DESKTOP-I2H2KJJ>
2018-01-26 10:09:32 +0800

    add detial back

---
+++ LUSSIS/App_Code/Detail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Detail
+/// </summary>
+public class Detail
+{
+    public int reqNo { get; set; }
+    public string description { get; set; }
+    public int? quantity { get; set; }
+    public Detail()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
