161e267406da3c40921f6db61cdf8a4c13208362
roman015 <xaviervarricatt@live.in>
2018-01-26 14:44:12 +0800

    WCF : Catalogue Complete

--- LUSSIS Backend/AndroidController.cs
+++ LUSSIS Backend/AndroidController.cs
@@ -16,7 +16,7 @@ namespace LUSSIS_Backend
                 .Where(collPoint => collPoint.CollectionPointNo == collectionPointNo)
                 .FirstOrDefault();
 
-            if(collectionPoint != null)
+            if (collectionPoint != null)
             {
                 result = collectionPoint.CollectionPointDetails;
             }
@@ -41,6 +41,35 @@ namespace LUSSIS_Backend
         {
             LussisEntities context = new LussisEntities();
             return context.Employees.Where(emp => emp.EmpNo.Equals(empNo)).FirstOrDefault();
+        }
+
+        public static List<StationeryCatalogue> GetCatalogue()
+        {
+            LussisEntities context = new LussisEntities();
+            return context.StationeryCatalogues.ToList();
+        }
+
+        public static List<StationeryCatalogue> SearchCatalogue(string itemNo, string category, string description, string bin)
+        {
+            LussisEntities context = new LussisEntities();
+
+            try
+            {
+                return context.StationeryCatalogues
+                    .Where( item =>
+                        item.ItemNo.Contains(itemNo)
+                        || item.Category.Contains(category)
+                        || item.Description.Contains(description)
+                        || item.Bin.Contains(bin)
+                    )
+                    .ToList();
+            }
+            catch (Exception e)
+            {
+                List<StationeryCatalogue> errorList = new List<StationeryCatalogue>();
+                errorList.Add(new StationeryCatalogue() { Description = e.Message.Substring(0,50) });
+                return errorList;
+            }
         }
     }
 }
