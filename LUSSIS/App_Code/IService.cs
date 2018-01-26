6b74f8671aa2375014d4bace164afee56d222033
roman015 <xaviervarricatt@live.in>
2018-01-26 10:57:05 +0800

    Adding Wcf to Main Project. Old one is not yet removed

---
+++ LUSSIS/App_Code/IService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+[ServiceContract]
+public interface IService
+{
+    // Test Function for android apps to check the connection to server.
+    // Always returns the string "test"
+    [OperationContract]
+    [WebGet(UriTemplate = "/test", ResponseFormat = WebMessageFormat.Json)]
+    string Test();
+
+    // Check if Session id is okay
+    [OperationContract]
+    [WebInvoke(UriTemplate = "/checkSession", Method = "POST", BodyStyle = WebMessageBodyStyle.WrappedRequest, ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+    bool TestSessionId(int sessionID);
+
+    [OperationContract]
+    [WebInvoke(UriTemplate = "/CollectionPoint", Method = "POST", BodyStyle = WebMessageBodyStyle.WrappedRequest, ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+    String GetCollectionPoint(int sessionID, int collectionPointNo);
+
+    [OperationContract]
+    [WebInvoke(UriTemplate = "/Department", Method = "POST", BodyStyle = WebMessageBodyStyle.WrappedRequest, ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+    WCFDepartment GetDepartment(int sessionID, String deptCode);
+}
+
+public class WCFDepartment
+{
+    public String DeptCode;
+    public String DeptName;
+    public String ContactName;
+    public String PhoneNo;
+    public String FaxNo;
+    public int HeadEmpNo;
+    public int CollectionPointNo;
+    public int RepEmpNo;
+    public int DeputyEmpNo;
+}
+
---
+++ LUSSIS/App_Code/Service.cs
@@ -0,0 +1,61 @@
+﻿using LUSSIS_Backend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IService
+{
+    public string Test()
+    {
+        return "test";
+    }
+
+    public bool TestSessionId(int sessionID)
+    {
+        return AndroidAuthenticationController.IsValidSessionId(sessionID);
+    }
+
+    public string GetCollectionPoint(int sessionID, int collectionPointNo)
+    {
+        String result = "";
+
+        if (AndroidAuthenticationController.IsValidSessionId(sessionID))
+        {
+            result = AndroidController.GetCollectionPointOf(collectionPointNo);
+        }
+
+        return result;
+    }
+
+    public WCFDepartment GetDepartment(int sessionID, String deptCode)
+    {
+        Department department = null;
+        WCFDepartment result = null;
+
+        if (AndroidAuthenticationController.IsValidSessionId(sessionID))
+        {
+            department = AndroidController.GetDepartment(deptCode);
+            if (department != null)
+            {
+                result = new WCFDepartment()
+                {
+                    CollectionPointNo = department.CollectionPointNo ?? -1,
+                    ContactName = department.ContactName,
+                    DeptCode = department.DeptCode,
+                    DeptName = department.DeptName,
+                    DeputyEmpNo = department.DeputyEmpNo ?? -1,
+                    FaxNo = department.FaxNo.ToString(),
+                    HeadEmpNo = department.HeadEmpNo ?? -1,
+                    PhoneNo = department.PhoneNo.ToString(),
+                    RepEmpNo = department.RepEmpNo ?? -1
+                };
+            }
+        }
+
+        return result;
+    }
+}
