5e2ae9a8ccf809430a5517b9a390dc8b2c3595af
collau <collau@hotmail.com>
2018-01-26 09:57:55 +0800

    Added POController and ViewPurchaseOrderHistory Page

---
+++ LUSSIS Backend/controller/PurchaseOrderController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LUSSIS_Backend.controller
+{
+    public class PurchaseOrderController
+    {
+        public static List<PurchaseOrderDetail> GetPurchaseOrderDetailsOf(int purchaseOrderNumber)
+        {
+            List<PurchaseOrderDetail> poDetails = new List<PurchaseOrderDetail>();
+            LussisEntities context = new LussisEntities();
+            poDetails = context.PurchaseOrderDetails.Where(po => po.PONo == purchaseOrderNumber).ToList();
+            return poDetails;
+        }
+    }
+}
