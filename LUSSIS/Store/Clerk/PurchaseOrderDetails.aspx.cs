e36650d52086a407e09e32194d8ac0c52b491505
ngchongpeng <ngchongpeng@hotmail.com>
2018-01-29 09:44:25 +0800

    Unignore Store Clerk Aspx

---
+++ LUSSIS/Store/Clerk/PurchaseOrderDetails.aspx.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using LUSSIS_Backend;
+using LUSSIS_Backend.controller;
+
+public partial class Store_Clerk_PurchaseOrderDetails : System.Web.UI.Page
+{
+    // ATTRIBUTES
+
+    protected int pONo;
+    protected PurchaseOrder pO;
+
+    // EVENTS
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            // If no PONo is passed, go back to List Page
+            if (Session["PONo"] == null)
+            {
+                GoToPurchaseOrderListPage();
+            }
+            else
+            {
+                // Set DataGrid
+                BindGrid();
+            }
+        }
+
+        // Set Everytime
+        pONo = (int)Session["PONo"];
+        pO = POController.GetPurchaseOrder(pONo);
+    }
+
+    protected void Button_Click(object sender, EventArgs e)
+    {
+        Button buttonPressed = (Button)sender;
+        pONo = (int)Session["PONo"];
+
+        // Delete Button
+        if (buttonPressed.CommandArgument == "Delete")
+        {
+            // Delete Purchase Order
+            try
+            {
+                POController.DeletePO(pONo);
+                Session["POProcessed"] = (int)Session["PONo"];
+            }
+            catch (Exception exception)
+            {
+                Session["Error"] = "An Error Has Occured: " + exception.Message;
+            }
+            if (Session["Error"] == null)
+            {
+                GoToPurchaseOrderListPage();
+            }
+        }
+    }
+
+    protected void OnRowEditing(object sender, GridViewEditEventArgs e)
+    {
+        PODetailsGridView.EditIndex = e.NewEditIndex;
+        BindGrid();
+    }
+
+    protected void OnRowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+        // Get Selected Row
+        GridViewRow row = PODetailsGridView.Rows[e.RowIndex];
+
+        try
+        {
+            string itemNo = (row.FindControl("LabelItemNo") as Label).Text;
+            int qty = Int32.Parse((string)e.NewValues["Qty"]);
+            POController.UpdatePODQty(pONo, itemNo, qty);
+            PODetailsGridView.EditIndex = -1;
+            BindGrid();
+        }
+        catch (Exception exception)
+        {
+            Session["Error"] = "An Error Has Occured: " + exception.Message;
+        }
+    }
+
+    protected void OnRowCancelingEdit(object sender, EventArgs e)
+    {
+        PODetailsGridView.EditIndex = -1;
+        BindGrid();
+    }
+
+    protected void OnRowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        GridViewRow row = PODetailsGridView.Rows[e.RowIndex];
+        string itemNo = (row.FindControl("LabelItemNo") as Label).Text;
+        POController.DeletePOD(pONo, itemNo);
+        BindGrid();
+    }
+
+    // METHODS
+
+    private void GoToPurchaseOrderListPage()
+    {
+        Response.Redirect("PurchaseOrderList.aspx");
+    }
+
+    private void BindGrid()
+    {
+        // Get PODetails
+        int pONo = (int)Session["PONo"];
+        List<PurchaseOrderDetail> pODs = POController.GetPODs(pONo);
+
+        // Set DataGrid
+        PODetailsGridView.DataSource = pODs.Select(
+            pOD => new
+            {
+                ItemNo = pOD.ItemNo,
+                ItemDescription = pOD.StationeryCatalogue.Description,
+                Qty = pOD.Qty
+            });
+
+        PODetailsGridView.DataBind();
+    }
+}
---
+++ LUSSIS/Store/Clerk/PurchaseOrderList.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using LUSSIS_Backend;
+using LUSSIS_Backend.controller;
+
+public partial class Store_Clerk_PurchaseOrderList : System.Web.UI.Page
+{
+    // ATTRIBUTES
+
+    List<PurchaseOrder> pendingPOs;
+
+    // METHODS
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            // Load GridView
+            BindGrid();
+        }
+    }
+
+    protected void PendingPurchaseOrderGridView_RowCommand(object sender, GridViewCommandEventArgs e)
+    {
+        int pONo;
+
+        if (e.CommandName == "PODetails")
+        {
+            pONo = Convert.ToInt32(e.CommandArgument);
+            Session["PONo"] = pONo;
+            Response.Redirect("PurchaseOrderDetails.aspx");
+        }
+    }
+
+    protected void PendingPurchaseOrderGridView_Delete(object sender, GridViewDeleteEventArgs e)
+    {
+        try
+        {
+            int pONo = Int32.Parse(PendingPurchaseOrderGridView.Rows[e.RowIndex].Cells[0].Text);
+            POController.DeletePO(pONo);
+            BindGrid();
+        }
+        catch (Exception exception)
+        {
+            Session["Error"] = "An Error Has Occured: " + exception.Message;
+        }
+    }
+
+    private void BindGrid()
+    {
+        // Get Pending Purchase Orders belonging to this User
+        pendingPOs = POController.GetPendingPOsByOrderedEmp(Profile.EmpNo);
+
+        // Set GridView
+        PendingPurchaseOrderGridView.DataSource = pendingPOs.Select(
+            pO => new
+            {
+                PONo = pO.PONo,
+                DateIssued = pO.DateIssued,
+                Supplier = pO.Supplier.SupplierName,
+                Status = pO.Status
+            });
+        PendingPurchaseOrderGridView.DataBind();
+    }
+}
